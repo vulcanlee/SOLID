6303055eebb946c6f229698fb6fb6678dc94de80
Vulcan <vulcan.lee@gmail.com>
2020-09-20 20:58:13 +0800

    請指出以下兩個類別哪些地方違反了 DIP 原則

--- SimpleExerciseDIP/SimpleExerciseDIP/Program.cs
+++ SimpleExerciseDIP/SimpleExerciseDIP/Program.cs
@@ -6,7 +6,26 @@ namespace SimpleExerciseDIP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SecurityService service = new SecurityService();
+            var isSuccess = service.LoginUser("Vulcan", "000");
+            Console.WriteLine("身分驗證"+(isSuccess==true?"成功":"失敗"));
+        }
+    }
+    public class SecurityService
+    {
+        public bool LoginUser(string userName, string password)
+        {
+            LoginService service = new LoginService();
+            return service.ValidateUser(userName, password);
+        }
+    }
+
+    public class LoginService
+    {
+        public bool ValidateUser(string userName, string password)
+        {
+            if (userName == "Vulcan" && password == "123") return true;
+            else return false;
         }
     }
 }
