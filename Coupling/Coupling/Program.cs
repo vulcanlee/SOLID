557b338eb06a4d8912f1c56575a6b55f95b3a037
Vulcan <vulcan.lee@gmail.com>
2020-09-20 12:36:46 +0800

    請指出這個類別的耦合特性為高還是低

---
+++ Coupling/Coupling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Coupling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Order
+    {
+        private ShoppingCartContents cart;
+        private float salesTax;
+
+        public Order(ShoppingCartContents cart, float salesTax)
+        {
+            this.cart = cart;
+            this.salesTax = salesTax;
+        }
+
+        public float OrderTotal()
+        {
+            float cartTotal = 0;
+            for (int i = 0; i < cart.items.Length; i++)
+            {
+                cartTotal += cart.items[i].Price * cart.items[i].Quantity;
+            }
+            cartTotal += cartTotal * salesTax;
+            return cartTotal;
+        }
+    }
+    public class ShoppingCartContents
+    {
+        public ShoppingCart[] items;
+    }
+
+    public class ShoppingCart
+    {
+        public float Price;
+        public int Quantity;
+    }
+}
