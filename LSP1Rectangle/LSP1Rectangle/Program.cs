1d29ee3bac09cd5e135ffd6811c55286fe266577
Vulcan <vulcan.lee@gmail.com>
2020-09-20 18:13:11 +0800

    第一版：設計矩形類別與計算面積

--- LSP1Rectangle/LSP1Rectangle/Program.cs
+++ LSP1Rectangle/LSP1Rectangle/Program.cs
@@ -6,17 +6,27 @@ namespace LSP1Rectangle
     {
         static void Main(string[] args)
         {
-            Rectangle o = new Rectangle();
-            o.Width = 40; o.Height = 50;
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 4; rectangle.Height = 5;
             Console.WriteLine($"Rectangle" +
-                $"({o.Width}x{o.Height}) area is " +
-                $"{o.GetArea()}");
+                $"({rectangle.Width}x{rectangle.Height}) area is " +
+                $"{rectangle.GetArea()}");
         }
     }
     public class Rectangle
     {
-        public int Height { get; set; }
-        public int Width { get; set; }
+        protected int _height;
+        protected int _width;
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
         public int GetArea()
         {
             return Height * Width;
