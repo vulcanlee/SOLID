7fbfe48735ee5191aee5bccba85bcd5a25e17889
Vulcan <vulcan.lee@gmail.com>
2020-09-20 20:03:19 +0800

    挑戰套用 LSP 原則 – 解法

---
+++ LSPFruitFix/LSPFruitFix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LSPFruitFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple();
+            Console.WriteLine($"Apple is {apple.GetColor()}");
+            Orange orange = new Orange();
+            Console.WriteLine($"Apple is {orange.GetColor()}");
+            Apple appleOrange = new Orange();
+            Console.WriteLine($"AppleOrange is {appleOrange.GetColor()}");
+        }
+    }
+    public class Apple
+    {
+        public virtual string GetColor() => "Red";
+    }
+    public class Orange : Apple
+    {
+        public override string GetColor() => "Orange";
+    }
+}
