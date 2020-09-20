4e1c9f2c5e1e476b3700efd27c618eb77d945e0b
Vulcan <vulcan.lee@gmail.com>
2020-09-20 13:51:40 +0800

    請試著修正該 OrderManager 類別，使其符合 單一責任原則

---
+++ GoodSRPOrderManager/GoodSRPOrderManager/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoodSRPOrderManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new OrderManager().Processing();
+        }
+    }
+    public class OrderManager
+    {
+        public List<Product> Products =
+                   new List<Product>();
+        public Customer Customer { get; set; }
+        public OrderManager()
+        {
+
+        }
+        public void Processing()
+        {
+            new Stock().CheckAvailability(
+                        Products);
+            new Payment().Processing(
+                        Customer, Products);
+            new Shipment().SendProducts(
+                        Customer, Products);
+        }
+    }
+    public class Product { }
+    public class Customer { }
+    public class Stock
+    {
+        public void CheckAvailability(
+                IEnumerable<Product> products)
+        { }
+    }
+    public class Payment
+    {
+        public void Processing(
+                Customer customer,
+        IEnumerable<Product> products)
+        { }
+    }
+    public class Shipment
+    {
+        public void SendProducts(
+                Customer customer,
+        IEnumerable<Product> products)
+        { }
+    }
+}
